cc17bfd823d91ca0478d47573b47535fa8b83455
Zenulous <zenvanriel@gmail.com>
2022-06-25 13:13:47 +0200

    Add description of Decorator

--- src/patterns/structural/decorator.ts
+++ src/patterns/structural/decorator.ts
@@ -1,3 +1,6 @@
+// Decorator
+// Attach additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality.
+
 class ProteinShake {
   calories = 200;
   proteinInGrams = 28;
